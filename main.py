f3236800e097fed1bfee12208053f55f02a1ae53
edmerckx99 <edmerckx99@yahoo.com>
2020-06-30 08:41:50 -0400

    Added player to screen

--- main.py
+++ main.py
@@ -8,15 +8,28 @@ screen = pygame.display.set_mode((800, 600))
 
 # Tile and Icon
 pygame.display.set_caption("Space Invaders")
-icon = pygame.image.load("ufo.png")
+icon = pygame.image.load('ufo.png')
 pygame.display.set_icon(icon)
+
+# Player
+playerImg = pygame.image.load('player.png')
+playerX = 370
+playerY = 480
+
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
 
 # Game loop
 running = True
 while running:
+
+    # RGB = Red, Green, Blue
+    screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((0, 0, 0))
+    player()
     pygame.display.update()
