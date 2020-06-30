cb9db6a5983db36b38ec620f52667bc479f4e37c
edmerckx99 <edmerckx99@yahoo.com>
2020-06-30 15:29:17 -0400

    Added player controls with Left and Right arrows

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ pygame.display.set_icon(icon)
 
 # Player
 playerImg = pygame.image.load('player.png')
-playerX = 370
-playerY = 480
+playerX = 370  # define the initial X value
+playerY = 480  # define the initial Y value
+playerX_change = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
 # Game loop
@@ -27,9 +28,26 @@ while running:
 
     # RGB = Red, Green, Blue
     screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    player()
+        # If keystroke is pressed check whether its right or left
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+
+    # Update the playerX position
+    playerX += playerX_change
+
+    # Add the player to the screen
+    player(playerX, playerY)
+
+    # Update the display
     pygame.display.update()
